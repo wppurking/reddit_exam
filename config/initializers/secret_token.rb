040fd9df0ae2b2e5dab500cbfb0e4f035987c9fe
wyatt <wppurking@gmail.com>
2013-11-04 14:57:07 +0800

    init commitˆ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditExam::Application.config.secret_key_base = '4a56027d28d1b614c37a47396815d527a3a0b979ee9d26c018ea89a929459145f4d48071887e168c2a8fe46e59139ced652451796a05341097d1b2ad5afe433c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RedditExam::Application.config.session_store :cookie_store, key: '_reddit_exam_session'
