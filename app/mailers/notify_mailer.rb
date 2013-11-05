38f8ded383dc600fa0cac49b12d5a14cec826eda
wyatt <wppurking@gmail.com>
2013-11-05 15:07:01 +0800

    * 添加 actionmailer 用于发送邮件.

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,10 +1,10 @@
 class NotifyMailer < ActionMailer::Base
-  default from: "wppurking@gmail.com"
+  default from: "Wyatt <wppurking@gmail.com>"
 
   def interview(line)
     args = line.split(',')
     @name = args[0]
-    @email = args[1]
+    @email = args[1].strip
     mail(to: @email, subject: '邀请参加长沙蓝拓电子商务有限公司笔试题')
   end
 
