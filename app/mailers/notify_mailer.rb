1b5c073845b4200e241ce0795907aae0b713da9b
wyatt <wppurking@gmail.com>
2013-11-13 12:46:09 +0800

    对邀请邮件增加内容

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -5,7 +5,7 @@ class NotifyMailer < ActionMailer::Base
     args = line.split(',')
     @name = args[0]
     @email = args[1].strip
-    mail(to: @email, subject: '邀请参加长沙蓝拓电子商务有限公司笔试题')
+    mail(to: @email, subject: "邀请 #{@name} 参加长沙蓝拓电子商务有限公司 Java 工程师笔试题")
   end
 
 end
